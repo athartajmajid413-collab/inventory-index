bc73265533a671b2e485db680cea7495f6ed7d47
athartajmajid413-collab <athartajmajid413@gmail.com>
2026-09-12 14:45:09 +0500

    Update Cost .js

--- Cost .js
+++ Cost .js
@@ -55,7 +55,47 @@ async function loadCostDataFromSupabase(){
 
         items =
             itemsResult.data || [];
+        // =================================
+// SORT ITEM CODE NUMERICALLY
+// SI 1, SI 2, SI 3 ... SI 10
+// =================================
 
+items.sort(function(a, b){
+
+    let codeA =
+        String(a.code || "").trim();
+
+    let codeB =
+        String(b.code || "").trim();
+
+
+    let numberA =
+        parseInt(
+            codeA.replace(/\D/g, ""),
+            10
+        );
+
+
+    let numberB =
+        parseInt(
+            codeB.replace(/\D/g, ""),
+            10
+        );
+
+
+    if(isNaN(numberA)){
+        numberA = Infinity;
+    }
+
+
+    if(isNaN(numberB)){
+        numberB = Infinity;
+    }
+
+
+    return numberA - numberB;
+
+});
 
         console.log(
             "Supabase Items:",
